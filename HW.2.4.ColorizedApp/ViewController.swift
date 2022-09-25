a31e2c3cea1fa95bf30829fecce22ecc6b46d324
ES <schahalinka@icloud.com>
2022-09-25 18:59:34 +0200

    Initial Commit

---
+++ HW.2.4.ColorizedApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW.2.4.ColorizedApp
+//
+//  Created by Елена Шахова on 25.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
